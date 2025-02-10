3f651240345d367ac7cfc9df3f216ccc323696dc
alenjzhang <alenjzhang@tencent.com>
2025-02-10 15:21:00 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className='text-red-700'>Vite + React</h1>
+      <h1 className='hover:text-red-300'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
